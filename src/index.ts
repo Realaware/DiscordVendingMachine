8a8232d8d93af4e265040af8468924c03fb9e833
RealReal <63380308+jiwonpaly@users.noreply.github.com>
2021-05-17 12:56:05 +0900

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Client from './Client';
+
+new Client().init();
+
+// bot developer is not taking responsbility of every action that user have conducted.
+// using bot illegally is not allowed.
